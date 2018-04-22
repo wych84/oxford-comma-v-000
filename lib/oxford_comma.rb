83e6e3a8eb8f700e363d66c0e47ff56e40b7dea2
Joyce Chun <wooyangee@gmail.com>
2018-04-22 03:44:54 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,7 +3,7 @@ if array.size == 1
   array.join
   elsif array.size == 2
   array.join(" and ")
-  elsif array.size >= 3 
+  else 
     last_element = array.pop
     new_string = array.join(", ")
     new_string << ", and #{last_element}"
