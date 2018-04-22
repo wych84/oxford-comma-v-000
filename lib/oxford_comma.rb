a4ac49375739fb192c9559575e393f554bd73a78
Joyce Chun <wooyangee@gmail.com>
2018-04-22 03:44:08 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,7 +3,7 @@ if array.size == 1
   array.join()
   elsif array.size == 2
   array.join(" and ")
-  elsif array.size == 3 
+  elsif array.size >= 3 
     last_word = array.pop
     new_string = array.join(", ")
     new_string << ", and #{last_word}"
