6d7dba69fad392200130bc2ab54209645f67cdb9
Joyce Chun <wooyangee@gmail.com>
2018-04-22 03:43:49 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,9 +1,9 @@
 def oxford_comma(array)
-if array.size <= 1
+if array.size == 1
   array.join()
   elsif array.size == 2
   array.join(" and ")
-  else 
+  elsif array.size == 3 
     last_word = array.pop
     new_string = array.join(", ")
     new_string << ", and #{last_word}"
